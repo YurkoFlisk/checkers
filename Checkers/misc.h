1cd035db6a8285ca308b2967484e5ff1e4202b3c
Yurko Prokopets <yurko.assimo@gmail.com>
2016-06-08 15:55:38 +0300

    added license mention

--- Checkers/misc.h
+++ Checkers/misc.h
@@ -1,7 +1,25 @@
-// Checkers
-// By Yurko Prokopets(aka YurkoFlisk)
-// misc.h
-// Version 1.3
+/*
+========================================================================
+Copyright (c) 2016 Yurko Prokopets(aka YurkoFlisk)
+
+This file is part of Checkers source code
+
+Checkers is free software : you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Checkers is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Checkers.If not, see <http://www.gnu.org/licenses/>
+========================================================================
+*/
+
+// misc.h, version 1.3
 
 #pragma once
 #ifndef _MISC_H
