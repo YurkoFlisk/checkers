af67abf7ef508e61bb742a349b19b77671f370d0
Yurko Prokopets <yurko.assimo@gmail.com>
2016-07-11 23:01:13 +0300

    working draft 1.6

--- CheckersEngTester/main.cpp
+++ CheckersEngTester/main.cpp
@@ -2,12 +2,13 @@
 // Launches engine vs engine game for automatical testing
 // different versions of engine against each other
 // Copyright(c) 2016 Yurko Prokopets(aka YurkoFlisk)
-// main.cpp, version 1.5
+// main.cpp, version 1.6
 
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <iostream>
 #include <string>
+#include <chrono>
 #include <fstream>
 #include "../Checkers/engine/board.h"
 #define CM_MOVE WM_USER
@@ -47,7 +48,10 @@ int main(void)
 		return 0;
 	whitePID = pi.dwThreadId, hWhite = pi.hProcess;
 	if (!CreateProcess(eng_black.data(), NULL, NULL, NULL, FALSE, NULL, NULL, NULL, &si, &pi))
+	{
+		TerminateProcess(hWhite, 0);
 		return 0;
+	}
 	blackPID = pi.dwThreadId, hBlack = pi.hProcess;
 	WaitForInputIdle(hWhite, INFINITE);
 	WaitForInputIdle(hBlack, INFINITE);
@@ -65,23 +69,29 @@ int main(void)
 		TerminateProcess(hBlack, 0);
 		return 0;
 	}
-	int wDepth, bDepth;
+	int wDepth, bDepth, timer;
 	cout << "Enter white depth: ";
 	cin >> wDepth;
 	cout << "Enter black depth: ";
 	cin >> bDepth;
 	SendMessage(hWndMsgWhite, CM_SETDEPTH, wDepth, NULL);
 	SendMessage(hWndMsgBlack, CM_SETDEPTH, bDepth, NULL);
+	std::ofstream log("cet_log.txt");
 	for (bool white_turn = true; ; white_turn = !white_turn)
 	{
+		auto start = chrono::high_resolution_clock::now();
 		SendMessage(white_turn ? hWndMsgWhite : hWndMsgBlack, CM_CPUMOVE, NULL, NULL);
+		auto end = chrono::high_resolution_clock::now();
+		timer = chrono::duration_cast<chrono::milliseconds>(end - start).count();
 		std::ifstream in("text.txt");
 		Board::read_move(in, move);
 		in.close();
 		gs = (game_state)(SendMessage(white_turn ? hWndMsgBlack : hWndMsgWhite, CM_MOVE, NULL, NULL));
 		cout << (white_turn ? "White move: " : "Black move: ");
 		Board::write_move(cout, move);
-		cout << '\n';
+		cout << ' ' << timer << "ms elapsed\n";
+		Board::write_move(log, move);
+		log << '\n';
 		if (gs != GAME_CONTINUE)
 		{
 			cout << (gs == WHITE_WIN ? "White won this game!\n" :
@@ -92,6 +102,7 @@ int main(void)
 			break;
 		}
 	}
+	log.close();
 	system("pause");
 	return 0;
 }
